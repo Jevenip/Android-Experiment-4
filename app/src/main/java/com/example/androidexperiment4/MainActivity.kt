71cbd170ce419d128c10e4041b163f2a20e2ed14
Jevenip <jever931249718@gmail.com>
2024-12-26 09:09:24 +0800

    feat:Intent

--- app/src/main/java/com/example/androidexperiment4/MainActivity.kt
+++ app/src/main/java/com/example/androidexperiment4/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.androidexperiment4
 
+import android.R
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        EditText editTextUrl = findViewById<EditText>(R.id.editTextUrl)
+        Button buttonOpen = findViewById<Button>(R.id.buttonOpen)
+        buttonOpen.setOnClickListener {
+            val url = editTextUrl.text.toString()
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse(url))
+            startActivity(intent)
+        }
+        enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
